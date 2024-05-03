c117edfe0a13873a6f26a79d38094976e326b414
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-05-03 16:47:48 +0200

    Create server.js to config

---
+++ server.js
@@ -0,0 +1,26 @@
+//Fichier principal de l'application Express.js où nous configurons notre serveur.
+// server.js
+
+const express = require('express');
+const app = express();
+const tarotRoutes = require('./routes/tarotRoutes');
+
+// Middleware pour traiter les requêtes JSON
+app.use(express.json());
+
+// Utilisation des routes pour les tirages de tarot
+app.use('/api/tarot', tarotRoutes);
+
+// Route de base pour afficher un message de bienvenue lorsque quelqu'un accède à la racine de l'API.
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l\'API du tarot en ligne');
+});
+
+// Port d'écoute du serveur
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});
+
+
+
