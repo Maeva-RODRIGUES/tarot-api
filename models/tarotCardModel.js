da40f0e3d1ec584d68f4013b499a3d65507426b5
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-05-21 15:29:01 +0200

    '# change name models'

---
+++ models/tarotCardModel.js
@@ -0,0 +1,9 @@
+// ce fichier contient les modèles sequelize definis suivants : TarotCard : Ce modèle représente les cartes de tarot et contient des champs pour le nom de la carte, sa description et sa position.
+// TarotDrawing : Ce modèle représente les tirages de tarot et contient un champ pour la date du tirage.
+// Défini les modeles sequelize
+
+
+
+
+
+ 
--- models/tarotModel.js
+++
@@ -1,9 +0,0 @@
-// ce fichier contient les modèles definis suivants : TarotCard : Ce modèle représente les cartes de tarot et contient des champs pour le nom de la carte, sa description et sa position.
-// TarotDrawing : Ce modèle représente les tirages de tarot et contient un champ pour la date du tirage.
-// Défini les modeles sequelize
-
-
-
-
-
- 
