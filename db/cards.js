593daeafb0ae56b782bd9cbcaf2df1e258fe3f3d
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-05-28 14:29:36 +0200

    Add db cards

---
+++ db/cards.js
@@ -0,0 +1,156 @@
+const cards = [
+    {
+        id: 0,
+        name_card: "Le Fou",
+        keyword1: "Innocence",
+        keyword2: "Nouvel départ",
+        keyword3: "Spontanéité"
+    },
+    {
+        id: 1,
+        name_card: "Le Magicien",
+        keyword1: "Créativité",
+        keyword2: "Action",
+        keyword3: "Pouvoir"
+    },
+    {
+        id: 2,
+        name_card: "La Papesse",
+        keyword1: "Intuition",
+        keyword2: "Sagesse",
+        keyword3: "Mystère"
+    },
+    {
+        id: 3,
+        name_card: "L'Impératrice",
+        keyword1: "Abondance",
+        keyword2: "Fécondité",
+        keyword3: "Nature"
+    },
+    {
+        id: 4,
+        name_card: "L'Empereur",
+        keyword1: "Autorité",
+        keyword2: "Stabilité",
+        keyword3: "Pouvoir"
+    },
+    {
+        id: 5,
+        name_card: "Le Pape",
+        keyword1: "Sagesse",
+        keyword2: "Tradition",
+        keyword3: "Conseil"
+    },
+    {
+        id: 6,
+        name_card: "Les Amoureux",
+        keyword1: "Amour",
+        keyword2: "Choix",
+        keyword3: "Harmonie"
+    },
+    {
+        id: 7,
+        name_card: "Le Chariot",
+        keyword1: "Volonté",
+        keyword2: "Triomphe",
+        keyword3: "Mouvement"
+    },
+    {
+        id: 8,
+        name_card: "La Justice",
+        keyword1: "Équilibre",
+        keyword2: "Vérité",
+        keyword3: "Justice"
+    },
+    {
+        id: 9,
+        name_card: "L'Hermite",
+        keyword1: "Introspection",
+        keyword2: "Sagesse",
+        keyword3: "Solitude"
+    },
+    {
+        id: 10,
+        name_card: "La Roue de Fortune",
+        keyword1: "Changement",
+        keyword2: "Destin",
+        keyword3: "Cyclicalité"
+    },
+    {
+        id: 11,
+        name_card: "La Force",
+        keyword1: "Courage",
+        keyword2: "Patience",
+        keyword3: "Compassion"
+    },
+    {
+        id: 12,
+        name_card: "Le Pendu",
+        keyword1: "Sacrifice",
+        keyword2: "Perspectives",
+        keyword3: "Détachement"
+    },
+    {
+        id: 13,
+        name_card: "La Mort",
+        keyword1: "Transformation",
+        keyword2: "Fin",
+        keyword3: "Renouveau"
+    },
+    {
+        id: 14,
+        name_card: "Tempérance",
+        keyword1: "Modération",
+        keyword2: "Équilibre",
+        keyword3: "Harmonie"
+    },
+    {
+        id: 15,
+        name_card: "Le Diable",
+        keyword1: "Attachement",
+        keyword2: "Tentations",
+        keyword3: "Illusions"
+    },
+    {
+        id: 16,
+        name_card: "La Maison Dieu",
+        keyword1: "Changement",
+        keyword2: "Révélation",
+        keyword3: "Catastrophe"
+    },
+    {
+        id: 17,
+        name_card: "L'Étoile",
+        keyword1: "Espoir",
+        keyword2: "Inspiration",
+        keyword3: "Sérénité"
+    },
+    {
+        id: 18,
+        name_card: "La Lune",
+        keyword1: "Illusion",
+        keyword2: "Intuition",
+        keyword3: "Mystère"
+    },
+    {
+        id: 19,
+        name_card: "Le Soleil",
+        keyword1: "Joie",
+        keyword2: "Succès",
+        keyword3: "Vitalité"
+    },
+    {
+        id: 20,
+        name_card: "Le Jugement",
+        keyword1: "Révélation",
+        keyword2: "Renaissance",
+        keyword3: "Évaluation"
+    },
+    {
+        id: 21,
+        name_card: "Le Monde",
+        keyword1: "Accomplissement",
+        keyword2: "Achèvement",
+        keyword3: "Intégration"
+    }
+];
