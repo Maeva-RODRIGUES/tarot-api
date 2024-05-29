bbcc1931018de4070b9fd38575db90743c0d0763
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2024-05-29 14:07:35 +0200

    Add cardsModels

---
+++ models/cardsModel.js
@@ -0,0 +1,37 @@
+// Ce fichier définit le modèle "Cards", qui représente les cartes de tarot dans la base de données. Il utilise Sequelize pour définir la structure des données et les contraintes associées.
+// Utilité: Permet de gérer les informations des cartes de tarot dans la base de données.
+
+const { DataTypes } = require('sequelize');
+// const sequelize = require('../');
+
+const Cards = sequelize.define('Card', {
+  name_card: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  keyword1: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  keyword2: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  keyword3: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  image_url: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Cards;
+
+
+
+
+
+
+ 
--- models/tarotCardModel.js
+++
@@ -1,9 +0,0 @@
-// Ce fichier contient les modèles sequelize definis suivants : TarotCard : Ce modèle représente les cartes de tarot et contient des champs pour le nom de la carte, sa description et sa position.
-// TarotDrawing : Ce modèle représente les tirages de tarot et contient un champ pour la date du tirage. à faire dans un autre fichier.
-// Défini les modeles sequelize : Cela doit refléter  la structure et les relations définies dans la base de données.Cela garantit que l’interaction entre l'API et la base de données est cohérente avec le schéma que j'ai conçu.
-
-
-
-
-
- 
